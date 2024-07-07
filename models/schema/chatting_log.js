f122aaf7c942b44eb46853d1df2b23aaf9f6f482
nueog2 <megoeun7378@naver.com>
2024-07-07 22:09:02 +0900

    FEAT : translated_question, translated_answer added

--- models/schema/chatting_log.js
+++ models/schema/chatting_log.js
@@ -15,14 +15,14 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-      // translated_question: {
-      //   type: DataTypes.TEXT,
-      //   allowNull: false,
-      // },
-      // translated_answer: {
-      //   type: DataTypes.TEXT,
-      //   allowNull: false,
-      // },
+      translated_question: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      translated_answer: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
       req_log_created: {
         type: DataTypes.INTEGER,
         allowNull: true,
