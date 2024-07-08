c947b5763e301a738766f8da5065b0d830d8bb1f
nueog2 <megoeun7378@naver.com>
2024-07-08 23:58:06 +0900

    HOTFIX

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "myapp",
+//       script: "app.js",
+//       watch: true,
+//       ignore_watch: ["node_modules", "req_thumb"],
+//       env: {
+//         NODE_ENV: "production",
+//       },
+//     },
+//   ],
+// };
