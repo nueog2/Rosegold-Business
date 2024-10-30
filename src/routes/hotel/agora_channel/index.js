ba5b744433df34727676d24952cd4b1604f18a22
nueog2 <megoeun7378@naver.com>
2024-10-30 21:52:48 +0900

    FEAT : agora_channel router complete

--- src/routes/hotel/agora_channel/index.js
+++ src/routes/hotel/agora_channel/index.js
@@ -2,5 +2,9 @@ const agoraChannelController = require("../../../controllers/agora_channel");
 const router = require("express").Router();
 
 router.post("/", agoraChannelController.createAgoraChannel);
+router.get("/many", agoraChannelController.readManyChannel);
+router.get("/one", agoraChannelController.readOneChannel);
+router.put("/", agoraChannelController.updateChannelName);
+router.put("/process", agoraChannelController.updateChannelProcess);
 
 module.exports = router;
