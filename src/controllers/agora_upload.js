0fecd2020e46ac9b1aece11116cd9644db2d9b39
nueog2 <megoeun7378@naver.com>
2024-11-05 17:53:02 +0900

    FEAT : agora upload _ url response added

--- src/controllers/agora_upload.js
+++ src/controllers/agora_upload.js
@@ -1,10 +1,133 @@
-// var admin = require("firebase-admin");
+var admin = require("firebase-admin");
+const { Buffer } = require("buffer");
+const moment = require("moment");
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 //   storageBucket: "gs://rosegold-app.appspot.com/customer",
 // });
-// gs://rosegold-app.appspot.com/customer
+// gs://rosegold-app.appspot.com/customer -> hotel.js 에 위치
+
+var bucket = admin.storage().bucket();
+
+// bucket.upload("/Users/megoe/Pictures/Screenshots/food1.png");
+
+// bucket.upload("/Users/megoe/Pictures/Screenshots/food1.png", {
+//   destination: "customer/food1.png", // 폴더 경로 포함
+// });
+
+// async function uploadCustomerFilestoFbStorage(req, res) {
+//   try {
+//     const hotelId = req.body.hotel_id;
+//     if (!hotelId) {
+//       return res.status(400).send("hotel_id is required.");
+//     }
+
+//     if (!req.files || !req.files.length) {
+//       return res.status(400).send("No files uploaded.");
+//     }
+
+//     const dateString = moment().format("YYMMDD"); // 날짜 문자열 생성
+//     const uploadedFiles = [];
+
+//     const uploadPromises = req.files.map(async (file) => {
+//       const destination = `customer/hotel${hotelId}/${dateString}/${file.originalname}`;
+//       const blob = bucket.file(destination);
+//       const blobStream = blob.createWriteStream({
+//         metadata: {
+//           contentType: file.mimetype,
+//         },
+//       });
+
+//       blobStream.on("error", (err) => {
+//         console.error("Error uploading file:", err);
+//         throw err;
+//       });
+
+//       blobStream.on("finish", async () => {
+//         const [metadata] = await blob.getMetadata();
+//         const downloadURL = metadata.mediaLink; // 다운로드 URL
+
+//         uploadedFiles.push({ name: file.originalname, url: downloadURL });
+//       });
+
+//       return new Promise((resolve, reject) => {
+//         blobStream.end(file.buffer, () => {
+//           resolve();
+//         });
+//       });
+//     });
+
+//     await Promise.all(uploadPromises);
+
+//     return res.status(200).json({ uploadedFiles });
+//   } catch (error) {
+//     console.error("Error uploading files:", error);
+//     res.status(500).send("Internal server error.");
+//   }
+// }
+
+async function uploadCustomerFilestoFbStorage(req, res) {
+  try {
+    const hotelId = req.body.hotel_id;
+    if (!hotelId) {
+      return res.status(400).send("hotel_id is required.");
+    }
+
+    if (!req.files || !req.files.length) {
+      return res.status(400).send("No files uploaded.");
+    }
+
+    const dateString = moment().format("YYMMDD");
+    const uploadedFiles = [];
+
+    const uploadPromises = req.files.map((file) => {
+      const destination = `customer/hotel${hotelId}/${dateString}/${file.originalname}`;
+      const blob = bucket.file(destination);
+      const blobStream = blob.createWriteStream({
+        metadata: {
+          contentType: file.mimetype,
+        },
+      });
+
+      return new Promise((resolve, reject) => {
+        blobStream.on("error", (err) => {
+          console.error("Error uploading file:", err);
+          reject(err);
+        });
+
+        blobStream.on("finish", async () => {
+          try {
+            const [metadata] = await blob.getMetadata();
+            const downloadURL = metadata.mediaLink;
+
+            await blob.setMetadata({
+              acl: [
+                {
+                  entity: "allUsers",
+                  role: "READER",
+                },
+              ],
+            });
+
+            uploadedFiles.push({ name: file.originalname, url: downloadURL });
+            resolve();
+          } catch (error) {
+            reject(error); // 메타데이터 가져오기 실패 시 reject
+          }
+        });
+
+        blobStream.end(file.buffer);
+      });
+    });
+
+    await Promise.all(uploadPromises);
+    return res.status(200).json({ uploadedFiles });
+  } catch (error) {
+    console.error("Error uploading files:", error);
+    return res.status(500).send("Internal server error."); // 에러 발생 시 500 에러 반환
+  }
+}
 
 // const message = require("../../config/message");
 // const { db, storageRef, getFirestore } = require("../firebase.js");
@@ -76,8 +199,8 @@
 // // 다른 컨트롤러 함수들
 // function otherControllerFunction1(req, res) {}
 
-// module.exports = {
-//   // addToFirestore,
-//   uploadCustomerFilestoFbStorage,
-//   otherControllerFunction1,
-// };
+module.exports = {
+  // addToFirestore,
+  uploadCustomerFilestoFbStorage,
+  // otherControllerFunction1,
+};
--- src/routes/hotel/agora_upload/index.js
+++ src/routes/hotel/agora_upload/index.js
@@ -1,10 +1,16 @@
 const router = require("express").Router();
 const multer = require("multer");
 const storage = multer.memoryStorage();
-// const upload = multer({ storage }); // dest 속성 제거 - 메모리 저장소 사용
+// const upload = multer({ storage });
 const agoraUploadController = require("../../../controllers/agora_upload.js");
-// const storage = multer.memoryStorage(); // 메모리에 파일 저장
-const upload = multer({ storage: storage }).array("files"); // 'files'는 input 필드의 name 속성
+// const storage = multer.memoryStorage();
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post(
+  "/upload",
+  upload.array("files"),
+  agoraUploadController.uploadCustomerFilestoFbStorage
+);
 
 // // 라우터에서 사용
 // router.post("/test", agoraUploadController.addToFirestore);
