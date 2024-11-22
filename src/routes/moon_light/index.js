cb6fcb1f1ec86d3b03e7f22ba2bf4444c3b0c6b2
nueog2 <megoeun7378@naver.com>
2024-11-22 23:58:52 +0900

    FEAT : moon_light chatting_log crud api complete

--- src/routes/moon_light/index.js
+++ src/routes/moon_light/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const storageRouter = require("./moonlight_storage");
 const moonlight_noticeRouter = require("./moonlight_notice");
+const moon_chatting_logRouter = require("./chatting_log");
 
 router.use("/notice", moonlight_noticeRouter);
 router.use("/storage", storageRouter);
+router.use("/chatting_log", moon_chatting_logRouter);
 
 module.exports = router;
