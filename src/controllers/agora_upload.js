00f53a1fe20bb58e7817c394385e9fd53c9fa429
nueog2 <megoeun7378@naver.com>
2024-11-27 18:31:55 +0900

    FEAT : save guest_name as list

--- src/controllers/agora_upload.js
+++ src/controllers/agora_upload.js
@@ -188,13 +188,14 @@ async function uploadCustomerFilestoFbStoragewithGuest(req, res) {
     });
 
     await Promise.all(uploadPromises);
-    nameJSON = [];
-    nameJSON.push(req.body.name);
+
+    const names = req.body.name.split(",").map((name) => name.trim());
+
     const moonlight_guest = new Moonlight_Guest();
     const response = await moonlight_guest.create(
       // await 추가
       hotelId,
-      nameJSON,
+      names,
       req.body.num_guest,
       req.body.room_id,
       req.body.process,
@@ -212,76 +213,6 @@ async function uploadCustomerFilestoFbStoragewithGuest(req, res) {
     return res.status(500).send("SERVER_INTERNAL_ERROR");
   }
 }
-
-// const message = require("../../config/message");
-// const { db, storageRef, getFirestore } = require("../firebase.js");
-// const { collection, addDoc } = require("firebase/firestore");
-
-// function uploadCustomerFilestoFbStorage(req, res) {
-//   if (!req.files || Object.keys(req.files).length === 0) {
-//     return res
-//       .status(message["400_BAD_REQUEST"].status)
-//       .send(message.issueMessage(message["400_BAD_REQUEST"], "FILE_NOT_FOUND"));
-//   }
-
-//   const uploadedFiles = [];
-
-//   const uploadPromises = []; // Promise들을 저장할 배열
-
-//   // 여러 파일 업로드 처리 (req.files가 객체일 경우)
-//   if (typeof req.files === "object") {
-//     for (const key in req.files) {
-//       const file = req.files[key];
-//       const fileRef = storageRef.child(file.name);
-//       const uploadPromise = uploadBytes(fileRef, file.data).then((snapshot) => {
-//         // 각 파일 업로드 후, URL을 가져와서 uploadedFiles 배열에 추가
-//         return snapshot.ref.getDownloadURL().then((url) => {
-//           uploadedFiles.push({
-//             name: snapshot.metadata.name,
-//             fullPath: snapshot.metadata.fullPath,
-//             url: url,
-//           });
-//         });
-//       });
-
-//       uploadPromises.push(uploadPromise);
-//     }
-//   } else {
-//     // 단일 파일 업로드 처리
-//     const file = req.files;
-//     const fileRef = storageRef.child(file.name);
-//     const uploadPromise = uploadBytes(fileRef, file.data).then((snapshot) => {
-//       return snapshot.ref.getDownloadURL().then((url) => {
-//         uploadedFiles.push({
-//           name: snapshot.metadata.name,
-//           fullPath: snapshot.metadata.fullPath,
-//           url: url,
-//         });
-//       });
-//     });
-//     uploadPromises.push(uploadPromise);
-//   }
-
-//   Promise.all(uploadPromises)
-//     .then(() => {
-//       res
-//         .status(message["200_SUCCESS"])
-//         // .send("FILE_UPLOAD_SUCCESS")
-//         .json({
-//           message: message["200_SUCCESS"].message, // message 객체에서 메시지 가져오기
-//           uploadedFiles,
-//         });
-//     })
-//     .catch((error) => {
-//       console.error("ERROR DURING UPLOADING FILES:", error);
-//       res
-//         .status(message["500_SERVER_INTERNAL_ERROR"])
-//         .send("FILE_UPLOAD_FAILED");
-//     });
-// }
-
-// // 다른 컨트롤러 함수들
-// function otherControllerFunction1(req, res) {}
 
 module.exports = {
   // addToFirestore,
